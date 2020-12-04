7d6f9a211040dad6e58eb5dce7ea1852cf4cb25b
Benjamin Kyhn <benkyhn@hotmail.com>
2020-12-04 19:43:37 +0100

    Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {Fragment} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
@@ -9,8 +9,19 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // Redux
 import {Provider} from 'react-redux';
 import store from './store';
+import setAuthToken from "./utils/setAuthToken";
+import {loadUser} from "./actions/auth";
 
-const App = () => (
+if (localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
+const App = () => {
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
+
+    return(
     <Provider store={store}>
         <Router>
             <Fragment>
@@ -26,6 +37,6 @@ const App = () => (
             </Fragment>
         </Router>
     </Provider>
-);
+)};
 
 export default App;
