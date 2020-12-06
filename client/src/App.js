83eddebffcf61e93799798355867c2814b2e5011
Benjamin Kyhn <benkyhn@hotmail.com>
2020-12-06 13:30:17 +0100

    Display profiles

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import AddEducation from "./components/profile-forms/AddEducation";
 import AddExperience from "./components/profile-forms/AddExperience";
+import Profiles from "./components/profiles/Profiles";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 // Redux
 import {Provider} from 'react-redux';
@@ -38,6 +39,7 @@ const App = () => {
                     <Switch>
                         <Route exact path="/register" component={Register}/>
                         <Route exact path="/login" component={Login}/>
+                        <Route exact path="/profiles" component={Profiles}/>
                         <PrivateRoute exact path="/dashboard" component={Dashboard}/>
                         <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
                         <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
